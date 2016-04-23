f3e88fab0542ba1d83d734614050a5fee7d6afb4
jetthreat <hamzah1faisal@gmail.com>
2016-04-23 20:45:18 +0400

    fixed main issues

--- help.lua
+++ help.lua
@@ -4,14 +4,14 @@ for k,v in pairs(myTable) do
   print(k,v)
   end
 answer=io.read("*n");
-if answer==1 then
-find_distance()
-end
 function find_distance()
 print("enter a value for v")
 ve = io.read("*n")
 print("enter a value for t")
 ti = io.read("*n")
 distance = ve*ti
-print("distance = " ..distance);
+print("The distance is "..distance)
+end
+if answer==1 then
+find_distance()
 end
